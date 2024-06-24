184b0449deaae493e7265a281d5f729ec03c14a5
Paz Constanza Gutiérrez Césped <paz.constanzagc@gmail.com>
2024-06-24 12:26:31 -0400

    avance de js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
+
 import data from './data/dataset.js';
 
 console.log(example, renderItems(data), data);
+
+let uls=renderItems(data)
+let info=document.getElementById("campeonas") 
+uls.forEach(ul => {
+    info.appendChild(ul)
+});
+
