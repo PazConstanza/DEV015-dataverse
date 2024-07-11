43fa1fdce38f1d7977c580e5ec034133791b7091
Gema Riffo <gema.r.y382@gmai.com>
2024-07-10 20:44:41 -0400

    avances filtros

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
-import { example } from './dataFunctions.js';
+//import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
-
 import data from './data/dataset.js';
-console.log(example, renderItems(data), data);
+
+
+
+//console.log( renderItems(data), data);
 let tarjetas = renderItems(data)  // Llama a "renderItems" para crear las tarjetas a partir de los datos
-let TotalTarjetas=document.getElementById("campeonas") // se obtiene el elemento del DOM donde se agregarán las tarjetas
+let TotalTarjetas = document.getElementById("campeonas") // se obtiene el elemento del DOM donde se agregarán las tarjetas
 tarjetas.forEach(tarjeta => {           // Añade cada tarjeta (ul) al contenedor en el DOM
     TotalTarjetas.appendChild(tarjeta)
 });
+
+
+//Los filtros se escuchan... pero no filtran :D falta la funcion
+document.addEventListener("DOMContentLoaded", function () {  //Filtro por dificultad
+    const filtroDif = document.getElementById("Dificultad");
+    filtroDif.addEventListener("change", function () {
+        const datoSeleccionado = filtroDif.value;
+        console.log(datoSeleccionado);
+    })
+})
+document.addEventListener("DOMContentLoaded", function () {  //Filtro por tipo de daño
+    const filtroDaño = document.getElementById("Daño");
+    filtroDaño.addEventListener("change", function () {
+        const datoSeleccionado = filtroDaño.value;
+        console.log(datoSeleccionado);
+    })
+})
+document.addEventListener("DOMContentLoaded", function () {  //Filtro por carril
+    const filtroCArril = document.getElementById("Carril");
+    filtroCArril.addEventListener("change", function () {
+        const datoSeleccionado = filtroCArril.value;
+        console.log(datoSeleccionado);
+    })
+})
