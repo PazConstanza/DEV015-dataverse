cec5b75f1843ac1f8c101a98e2bb345a341fcf4a
Paz Constanza Gutiérrez Césped <paz.constanzagc@gmail.com>
2024-07-19 20:34:15 -0400

    arreglo de filtro fisico

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -2,35 +2,36 @@
 
 
 export const filterData = (data, filterBy, value) => {
-    console.log(filterBy);
-    const dataFiltrada = data.filter(campeona => campeona.hechos[filterBy] === value);
-      console.log(dataFiltrada)
-    return dataFiltrada;
-  };
-  
-  export const sortData = (data, sortBy, sortOrder) => {
-    if (sortOrder === "asc") {
-      return data.sort((a, b) => {
-        if (a[sortBy] > b[sortBy]) {
-          return 1;
-        } else if (a[sortBy] < b[sortBy]) {
-          return -1;
-        } else {
-          return 0;
-        }
-      });
-    } else if (sortOrder === "desc") {
-      return data.sort((a, b) => {
-        if (a[sortBy] > b[sortBy]) {
-          return -1;
-        } else if (a[sortBy] < b[sortBy]) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
-    } else {
-      
-      return data;
-    }
-  };
+  console.log(filterBy);
+  const dataFiltrada = data.filter(campeona => campeona.hechos[filterBy] === value);
+  console.log(dataFiltrada)
+  return dataFiltrada;
+};
+
+export const sortData = (data, sortBy, sortOrder) => {
+  let dataOrdenada = data
+  if (sortOrder === "asc") {
+    return dataOrdenada.sort((a, b) => {
+      if (a[sortBy] > b[sortBy]) {
+        return 1;
+      } else if (a[sortBy] < b[sortBy]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortOrder === "desc") {
+    return dataOrdenada.sort((a, b) => {
+      if (a[sortBy] > b[sortBy]) {
+        return -1;
+      } else if (a[sortBy] < b[sortBy]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else {
+
+    return dataOrdenada;
+  }
+};
